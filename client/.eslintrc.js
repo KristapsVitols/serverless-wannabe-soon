4f1c08a5f80b55d1f0c47a1965b14babab458d44
kristapsv <kristapsvitols91@gmail.com>
2020-01-01 14:03:53 +0200

    re-did client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,15 +5,14 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript'
+    'eslint:recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: 'babel-eslint'
   },
   overrides: [
     {
